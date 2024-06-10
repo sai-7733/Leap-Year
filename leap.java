08036790fecf4b03958c005a7aeb7e141200d4a3
Akula Chalamannagari Sai <akulachalamannagarisai@gmail.com>
2024-06-10 09:40:28 +0530

    Add files via upload

---
+++ leap.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class A{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if((n%4==0 && n%100!=0) || (n%400==0)){
+            System.out.println("Leap Year");
+        }
+        else{
+            System.out.println("Not Leap Year");
+        }
+    }
+}
